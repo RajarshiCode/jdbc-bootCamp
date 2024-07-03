f54620bf9cb7400a85c8aa48427eda84b8719d2c
RajarshiCode <rajarshidas47@gmail.com>
2024-07-03 11:25:39 +0530

    did 4 crud operation with preparedStatements

--- Main.java
+++ Main.java
@@ -17,14 +17,41 @@ public class Main {
         try{
 
             Connection connection = DriverManager.getConnection(url,username,password);
-            Statement statement = connection.createStatement();
+
+//            String query = "insert into students(name, age, marks) values(?,?,?)";
+            //String query = "select marks from students where id = ?";
+            String query = "update students set age = ? where id= ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            //String query = "delete from students where id = ?";
+            // preparedStatement.setInt(1, 4); // id o. will get deleted.
+    //          preparedStatement.setInt(1,4);
+                preparedStatement.setInt(1,30);
+                preparedStatement.setInt(2,4);
+
+//            preparedStatement.setString(1, "Archisman");
+//            preparedStatement.setInt(2, 24);
+//            preparedStatement.setDouble(3,82.6);
+            //Statement statement = connection.createStatement();
             // String query = "select * from students";
 //            String query = String.format("insert into students(name, age, marks) values('%s', %o, %f)", "Samrat",20, 98.5);
 //            String query = String.format("update students set marks = %f where id = %o", 89.5, 3);
 //            ResultSet resultSet = statement.executeQuery(query);
-            String query = "delete from students where id = 3";
-            int rowsAffected = statement.executeUpdate(query);
+//            String query = "delete from students where id = 3";
+            //int rowsAffected = preparedStatement.executeUpdate(query);
 
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            if(resultSet.next())
+//            {
+//                //System.out.println("marks: " + resultSet.getDouble("marks"));
+//                double marks = resultSet.getDouble("marks");
+//                System.out.println("Marks : "+ marks);
+//            }else{
+//                System.out.println("Marks not found!");
+//            }
+
+
+
+            int rowsAffected = preparedStatement.executeUpdate();
             if(rowsAffected>0)
                 System.out.println("Data Inserted!");
             else
